9ea0c6d2166bd38fa58094b1c171047d9063cd65
bawumia23 <bawumia91@gmail.com>
2026-03-01 12:56:34 +0000

    Rewrite files to be Betty-compliant and add README

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,15 +7,14 @@
  */
 int main(void)
 {
-    char str[] = "_putchar\n";
-    int i = 0;
+	char str[] = "_putchar\n";
+	int i = 0;
 
-    while (str[i] != '\0')
-    {
-        _putchar(str[i]);
-        i++;
-    }
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
 
-    return (0);
+	return (0);
 }
-
