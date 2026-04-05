92dac48aa9f4bdcac10504894506f2157e7b599a
bawumia23 <bawumia91@gmail.com>
2026-04-05 08:10:17 +0000

    Fix argument count (exclude program name)

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - prints number of arguments
- * @argc: argument count
- * @argv: argument vector
+ * main - prints the number of arguments passed to the program
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * Return: 0
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
 	(void)argv;
 
 	printf("%d\n", argc - 1);
+
 	return (0);
 }
