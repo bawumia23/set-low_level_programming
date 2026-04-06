94fa974258f98a731ed2cd56dc11bc334c09de5d
bawumia23 <bawumia91@gmail.com>
2026-04-06 07:38:56 +0000

    fix

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,18 +1,6 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer using _putchar
- * @n: number to print
- */
-void print_number(int n)
-{
-	if (n / 10)
-		print_number(n / 10);
-
-	_putchar((n % 10) + '0');
-}
-
-/**
  * main - prints number of arguments passed into program
  * @argc: argument count
  * @argv: argument vector
@@ -23,7 +11,7 @@ int main(int argc, char *argv[])
 {
 	(void)argv;
 
-	print_number(argc - 1);
+	_putchar((argc - 1) + '0');
 	_putchar('\n');
 
 	return (0);
