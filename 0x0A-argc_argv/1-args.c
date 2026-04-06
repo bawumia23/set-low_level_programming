9eb19fefce81dea7660303d23fb23c12aa50a2a7
bawumia23 <bawumia91@gmail.com>
2026-04-06 09:51:16 +0000

    Fix: correctly count arguments by subtracting program name

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,20 +1,13 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer
- * @n: integer to be printed
+ * print_number - prints an integer using _putchar
+ * @n: the integer to print
  */
 void print_number(int n)
 {
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-	}
-
 	if (n / 10)
 		print_number(n / 10);
-
 	_putchar((n % 10) + '0');
 }
 
@@ -23,13 +16,13 @@ void print_number(int n)
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: 0
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-	/* argc - 1 gives the number of arguments excluding the program name */
-	print_number(argc);
-	_putchar('\n'); 
+	/* We subtract 1 to exclude the program name from the count */
+	print_number(argc - 1);
+	_putchar('\n');
 
 	return (0);
 }
