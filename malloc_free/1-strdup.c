ce383b8be3d43ca8aec495fbfdc158f96ac662b1
bawumia23 <bawumia91@gmail.com>
2026-05-11 15:25:52 +0000

    Complete malloc_free tasks 0 to 4

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicates a string
+ * @str: string to duplicate
+ *
+ * Return: pointer to duplicated string
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	int i, len;
+
+	if (str == NULL)
+		return (NULL);
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	dup = malloc(sizeof(char) * (len + 1));
+
+	if (dup == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+	{
+		dup[i] = str[i];
+	}
+
+	dup[len] = '\0';
+
+	return (dup);
+}
---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a 2D grid
+ * @grid: grid to free
+ * @height: height of grid
+ *
+ * Return: nothing
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
