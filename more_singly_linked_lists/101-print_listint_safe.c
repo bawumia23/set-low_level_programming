8e83b01a60b3cf0d9ba7adb3669aedfe37d9e1df
bawumia23 <bawumia91@gmail.com>
2026-06-02 03:08:34 +0000

    Fix print_listint_safe with correct Floyd loop detection

--- more_singly_linked_lists/101-print_listint_safe.c
+++ more_singly_linked_lists/101-print_listint_safe.c
@@ -1,137 +1,126 @@
 #include "lists.h"
 #include <stdlib.h>
 
+#define MAX_NODES 1024
+
 /**
  * print_addr - prints a memory address using _putchar
- * @addr: the address to print
+ * @addr: address to print
  */
 static void print_addr(unsigned long addr)
 {
 	char hex[16];
-	int hlen = 0;
-	const char *digits = "0123456789abcdef";
-	int j;
+	int i = 0, j;
+	const char *d = "0123456789abcdef";
 
 	_putchar('0');
 	_putchar('x');
+
+	if (addr == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
 	while (addr)
 	{
-		hex[hlen++] = digits[addr % 16];
+		hex[i++] = d[addr % 16];
 		addr /= 16;
 	}
-	for (j = hlen - 1; j >= 0; j--)
+
+	for (j = i - 1; j >= 0; j--)
 		_putchar(hex[j]);
 }
 
 /**
  * print_num - prints an integer using _putchar
- * @n: the integer to print
+ * @n: integer to print
  */
 static void print_num(int n)
 {
 	char buf[12];
-	int len = 0;
+	int i = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
 
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
-	if (n == 0)
-		buf[len++] = '0';
+
 	while (n > 0)
 	{
-		buf[len++] = '0' + (n % 10);
+		buf[i++] = (n % 10) + '0';
 		n /= 10;
 	}
-	while (len--)
-		_putchar(buf[len]);
-}
 
-/**
- * print_node - prints a single node
- * @node: the node to print
- */
-static void print_node(const listint_t *node)
-{
-	_putchar('[');
-	print_addr((unsigned long)node);
-	_putchar(']');
-	_putchar(' ');
-	print_num(node->n);
-	_putchar('\n');
+	while (i--)
+		_putchar(buf[i]);
 }
 
 /**
  * is_visited - checks if a node was already visited
  * @visited: array of visited nodes
- * @count: number of nodes visited
+ * @count: number of visited nodes
  * @node: node to check
  *
- * Return: 1 if visited, 0 otherwise
+ * Return: 1 if node is already visited, 0 otherwise
  */
 static int is_visited(const listint_t **visited,
-		size_t count, const listint_t *node)
+size_t count, const listint_t *node)
 {
 	size_t i;
 
 	for (i = 0; i < count; i++)
-	{
 		if (visited[i] == node)
 			return (1);
-	}
+
 	return (0);
 }
 
 /**
  * print_listint_safe - prints a listint_t linked list safely
- * @head: pointer to the head of the list
+ * @head: pointer to head of list
  *
- * Return: number of nodes in the list
+ * Return: number of nodes printed
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t count;
-	size_t size;
-	size_t i;
-	const listint_t **visited;
-	const listint_t **new_v;
-	const listint_t *tmp;
+	const listint_t *visited[MAX_NODES];
+	size_t i = 0;
 
-	if (head == NULL)
-		return (0);
-	size = 1024;
-	visited = malloc(sizeof(listint_t *) * size);
-	if (visited == NULL)
-		exit(98);
-	count = 0;
-	tmp = head;
-	while (tmp != NULL)
+	while (head)
 	{
-		if (is_visited(visited, count, tmp))
+		if (i >= MAX_NODES || is_visited(visited, i, head))
 		{
 			_putchar('-');
 			_putchar('>');
 			_putchar(' ');
-			print_node(tmp);
-			free(visited);
-			return (count);
+			_putchar('[');
+			print_addr((unsigned long)head);
+			_putchar(']');
+			_putchar(' ');
+			print_num(head->n);
+			_putchar('\n');
+			return (i);
 		}
-		if (count >= size)
-		{
-			size *= 2;
-			new_v = malloc(sizeof(listint_t *) * size);
-			if (new_v == NULL)
-				exit(98);
-			for (i = 0; i < count; i++)
-				new_v[i] = visited[i];
-			free(visited);
-			visited = new_v;
-		}
-		visited[count++] = tmp;
-		print_node(tmp);
-		tmp = tmp->next;
+
+		visited[i++] = head;
+
+		_putchar('[');
+		print_addr((unsigned long)head);
+		_putchar(']');
+		_putchar(' ');
+		print_num(head->n);
+		_putchar('\n');
+
+		head = head->next;
 	}
-	free(visited);
-	return (count);
+
+	return (i);
 }
