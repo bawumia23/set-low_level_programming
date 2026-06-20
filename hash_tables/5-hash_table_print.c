064dac8e86bd49434ee66b5d9f9a90c23184af22
bawumia23 <bawumia91@gmail.com>
2026-06-20 12:53:16 +0000

    Hash tables: implement create, djb2, key_index, set, get, print, delete

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+#include <stdio.h>
+
+/**
+ * hash_table_print - Prints a hash table
+ * @ht: The hash table to print
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node;
+	int first;
+
+	if (ht == NULL)
+		return;
+
+	first = 1;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			if (first == 0)
+				printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			first = 0;
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
